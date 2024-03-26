4e9401a2489bde4734c2da1ffc2df6c928584802
birmain <ebirmain@gmail.com>
2024-03-25 21:45:15 -0600

    add app settings var

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Get the Configuration Service.
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
